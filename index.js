79740080081d6b095fb9cf4ec54dcfb720f75953
rulewrite <rulewrite@gmail.com>
2024-11-26 00:23:01 +0900

    feat: 엘리먼트 props 노드 속성으로 바인딩

--- index.js
+++ index.js
@@ -25,11 +25,21 @@ function createTextElement(text) {
   };
 }
 
+function isProperty(key) {
+  return key !== 'children';
+}
+
 function render(element, container) {
   const dom =
     element.type == 'TEXT_ELEMENT'
       ? document.createTextNode('')
       : document.createElement(element.type);
+
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
 
   element.props.children.forEach((child) => {
     render(child, dom);
