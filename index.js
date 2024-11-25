bb469c63220e8315204d63c104c0d643921ba55d
rulewrite <rulewrite@gmail.com>
2024-11-25 23:50:48 +0900

    feat: 객체를 반환하는 createElement 기본 스펙 구현

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function createElement(
+  type,
+  props,
+  // rest 매개변수이므로 항시 배열
+  ...children
+) {
+  return {
+    type,
+    props: {
+      ...props,
+      children,
+    },
+  };
+}
+
 const element = React.createElement(
   'div',
   { id: 'foo' },
