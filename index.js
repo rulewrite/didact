500a8884183eab229cc22e55ab6cb1ec41964d0c
rulewrite <rulewrite@gmail.com>
2024-11-25 23:52:54 +0900

    feat: 문자열, 숫자 등 원시 값 children을 위한 TEXT_ELEMENT 타입 객체 추가 및 분기

--- index.js
+++ index.js
@@ -11,7 +11,19 @@ function createElement(
     type,
     props: {
       ...props,
-      children,
+      children: children.map((child) => {
+        return typeof child === 'object' ? child : createTextElement(child);
+      }),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
     },
   };
 }
