8c1c1e2668e405136bc669a0e839014c2dc162b5
rulewrite <rulewrite@gmail.com>
2025-01-07 00:47:31 +0900

    refactor: ts 파일로 변환

--- src/index.ts
+++ src/index.ts
@@ -271,59 +271,8 @@ function reconcileChildren(wipFiber, elements) {
   }
 }
 
-const Didact = {
+export default {
   createElement,
   render,
   useState,
 };
-
-// https://github.com/parcel-bundler/parcel/issues/7234#issuecomment-1130291538
-/** @jsxRuntime classic @jsx Didact.createElement */
-function App(props) {
-  return <h1>Hi {props.name}</h1>;
-}
-
-function Counter() {
-  const [state, setState] = Didact.useState(1);
-  return <h1 onClick={() => setState((c) => c + 1)}>Count: {state}</h1>;
-}
-
-const container = document.getElementById('root');
-
-const updateValue = (e) => {
-  rerender(e.target.value);
-};
-
-const rerender = (value) => {
-  const element = (
-    <div id="foo">
-      <App name="timothy" />
-      <Counter />
-
-      <div>
-        <input onInput={updateValue} value={value} />
-        <h2>input value: {value}</h2>
-      </div>
-
-      <div>
-        hello
-        <div>
-          hello
-          <div>
-            hello
-            <div>
-              hello
-              <div>
-                hello
-                <div>hello</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-  Didact.render(element, container);
-};
-
-rerender('World');
