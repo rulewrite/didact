a19cebf8865d69065736d24ac7bf9f8906398be5
rulewrite <rulewrite@gmail.com>
2025-01-14 00:28:25 +0900

    chore: 렌더 단계 주석 추가

--- src/render.ts
+++ src/render.ts
@@ -12,20 +12,24 @@ export function render(
     return;
   }
 
+  // DOM 생성
   const dom =
     element.type === 'TEXT_ELEMENT'
       ? document.createTextNode('')
       : document.createElement(element.type);
 
+  // 프로퍼티 바인딩
   Object.keys(element.props)
     .filter(isProperty)
     .forEach((name) => {
       (dom as any)[name] = element.props[name];
     });
 
+  // 자식 엘리먼트 렌더
   element.props.children.forEach((child) => {
     render(child, dom);
   });
 
+  // 컨테이너에 바인딩
   container.appendChild(dom);
 }
