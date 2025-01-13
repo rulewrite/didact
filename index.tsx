b0a8fcdad92a9c9cf2f3d852188a6c5d84ef27b2
rulewrite <rulewrite@gmail.com>
2025-01-13 23:46:44 +0900

    feat: 초기 코드로 변경

--- index.tsx
+++ index.tsx
@@ -1,22 +1,13 @@
 // 리액트 엘리먼트 정의
-const element: DidactElement = {
-  type: 'h1',
-  props: {
-    title: 'foo',
-    children: 'Hello',
-  },
-};
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+);
 
 // DOM에서 노드 가져오기
 const container = document.getElementById('root');
 
 // 리액트 엘리먼트를 컨테이너에 렌더링
-// 혼동을 피하기 위해 리액트 요소는 엘리먼트, DOM 요소는 node라 함.
-const node = document.createElement(element.type);
-node['title'] = element.props.title;
-
-const text = document.createTextNode('');
-text['nodeValue'] = element.props.children;
-
-node.appendChild(text);
-container?.appendChild(node);
+ReactDOM.render(element, container);
