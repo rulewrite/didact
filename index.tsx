ee341c1b6f12e0ad561df04af97664ea0f825f50
rulewrite <rulewrite@gmail.com>
2025-01-14 00:05:35 +0900

    refactor: createElement 추가 및 대체

--- index.tsx
+++ index.tsx
@@ -1,9 +1,15 @@
+import { createElement } from './src/createElement';
+
+const Didact = {
+  createElement,
+};
+
 // 리액트 엘리먼트 정의
-const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
-  </div>
+const element = Didact.createElement(
+  'div',
+  { id: 'foo' },
+  Didact.createElement('a', null, 'bar'),
+  Didact.createElement('b')
 );
 
 // DOM에서 노드 가져오기
