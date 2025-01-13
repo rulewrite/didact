b2de7fa6937a9e33bfe52d8833d318cfa6095e34
rulewrite <rulewrite@gmail.com>
2025-01-13 23:35:35 +0900

    feat: jsx > js (createElement)

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 // 리액트 엘리먼트 정의
-const element = <h1 title="foo">Hello</h1>;
+const element = React.createElement('h1', { title: 'foo' }, 'Hello');
 
 // DOM에서 노드 가져오기
 const container = document.getElementById('root');
