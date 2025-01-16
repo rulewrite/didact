49af6ac2b2c48f459f6582fae6fb78babd9608f4
rulewrite <rulewrite@gmail.com>
2025-01-16 21:03:00 +0900

    feat: 상태 훅 있기 전 임시 재렌더 로직을 상태 훅으로 대체

--- index.tsx
+++ index.tsx
@@ -12,17 +12,6 @@ const Didact = {
 /** @jsxRuntime classic @jsx Didact.createElement */
 
 // 리액트 엘리먼트를 컨테이너에 렌더링
-const updateValue = (event: any) => {
-  rerender(event.target.value);
-};
-
-let isShow = 0;
-const removeItem = () => {
-  ++isShow;
-
-  rerender('delete');
-};
-
 function Component(props: any) {
   /*
   jsx > js로 변환 시
@@ -47,34 +36,56 @@ function Counter() {
   return <h1 onClick={() => setState((count) => count + 1)}>Count: {state}</h1>;
 }
 
-// DOM에서 노드 가져오기
-const container = document.getElementById('root');
+function App() {
+  const [text, setText] = Didact.useState('');
+  const [deleteCount, setDeleteCount] = Didact.useState(0);
 
-const rerender = (value: any) => {
-  const element = (
+  return (
     <div>
-      <Component name="티모시" />
-      <Counter />
-      <ul>
-        {isShow < 1 ? <li>삭제대상1</li> : null}
-        <li>첫번째{isShow}</li>
-        {isShow < 2 ? <li>삭제대상2</li> : null}
-        <li>세번째{isShow}</li>
-        {isShow < 3 ? <li>삭제대상3</li> : null}
-      </ul>
-
-      <button type="button" onClick={removeItem}>
+      <button
+        type="button"
+        onClick={() => {
+          setDeleteCount((_deleteCount) => {
+            return _deleteCount + 1;
+          });
+        }}
+      >
         delete
       </button>
 
+      <ul>
+        {deleteCount < 1 ? <li>삭제대상1</li> : null}
+        <li>첫번째{deleteCount}</li>
+        {deleteCount < 2 ? <li>삭제대상2</li> : null}
+        <li>세번째{deleteCount}</li>
+        {deleteCount < 3 ? <li>삭제대상3</li> : null}
+      </ul>
+
       <hr />
 
-      <input onInput={updateValue} value={value} />
-      <h2>Hello {value}</h2>
+      <input
+        onInput={(event: any) => {
+          setText(() => {
+            return event.target.value;
+          });
+        }}
+        value={text}
+      />
+      <h2>Hello {text}</h2>
     </div>
   );
+}
 
-  Didact.render(element, container);
-};
+const element = (
+  <div>
+    Hello, Didact
+    <Component name="티모시" />
+    <Counter />
+    <App />
+  </div>
+);
 
-rerender('World');
+// DOM에서 노드 가져오기
+const container = document.getElementById('root');
+
+Didact.render(element, container);
